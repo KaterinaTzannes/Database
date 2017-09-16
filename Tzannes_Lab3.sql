72844cb6d1b09289bb9fe51bc7e62cc0ca9bdd59
KaterinaTzannes <katerina.tzannes1@marist.edu>
2017-09-16 15:00:13 -0400

    Katerina Tzannes 9/16/17 Lab 3

---
+++ Tzannes_Lab3.sql
@@ -0,0 +1,29 @@
+﻿-- Katerina Tzannes
+-- Lab 3
+
+select ordno, totalUSD
+from orders;
+
+select name, city
+from agents where name = 'Smith';
+
+select pid, name, priceUSD
+from products where qty > 200010;
+
+select name, city
+from agents where city = 'Duluth';
+
+select name, city
+from agents where city != 'New York' AND city != 'Duluth';
+
+select *
+from products where city != 'Dallas' AND city != 'Duluth' AND priceUSD > 1.00;
+
+select *
+from orders where month = 'Mar' OR month = 'May';
+
+select *
+from orders where month = 'Feb' AND totalUSD > 500;
+
+select *
+from customers where cid = 'c005';
