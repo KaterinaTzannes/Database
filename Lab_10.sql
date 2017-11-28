9aedfa9f18038b89ee7110f509f23dc7c88c5e21
KaterinaTzannes <katerina.tzannes1@marist.edu>
2017-11-28 13:23:09 -0500

    Lab 10 11/28/17 Katerina Tzannes

---
+++ Lab_10.sql
@@ -0,0 +1,47 @@
+﻿create or replace function coursesCredits(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   numberOfCredits int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select num, name, credits
+      from   courses
+       where  credits >= numberOfCredits;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select coursesCredits(0, 'results');
+Fetch all from results;
+
+create or replace function PreReqsFor(courseNum) returns refcursor as 
+$$
+declare
+   numberOfCredits int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+	Select *
+	from courses
+	where num >= credits
+	return resultset;
+end;
+$$ 
+language plpgsql;
+	
+create or replace function IsPreReqFor(courseNum) returns refcursor as 
+$$
+declare
+   numberOfCredits int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+	select *
+	from courses
+	where num <= credits
+	return resultset;
+end;
+$$ 
+language plpgsql;
